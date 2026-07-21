b43aad257fde246fcf1496d2c3c1d7d7bc9cceeb
Keshav Sharma <keshavsharma904517@gmail.com>
2026-07-21 10:36:01 +0530

    feat: 5. Cart State Management setup

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+export const initialState = {
+  basket: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    // Other actions will be added later
+    default:
+      return state;
+  }
+};
+
+export default reducer;
