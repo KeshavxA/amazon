bb9c5429df129964e4f36b068b1af884927346ac
Keshav Sharma <keshavsharma904517@gmail.com>
2026-07-24 10:31:37 +0530

    feat: 2. Continue Shopping Button on Orders Page

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Orders.css';
 import { useStateValue } from "../StateProvider";
+import { Link } from 'react-router-dom';
 
 function Orders() {
   const [{ user }] = useStateValue();
@@ -11,12 +12,16 @@ function Orders() {
 
       <div className='orders__order'>
         {user ? (
-          <p>Thank you, {user.email}! Your order has been placed successfully and will be shipped soon.</p>
+          <p>Thank you, {user.email.split('@')[0]}! Your order has been placed successfully and will be shipped soon.</p>
         ) : (
           <p>Thank you for your order! Please sign in to save your order history.</p>
         )}
         <p className="orders__id">Order #123456789</p>
       </div>
+
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <button className='orders__continueButton'>Continue Shopping</button>
+      </Link>
     </div>
   )
 }
